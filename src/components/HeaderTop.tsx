36114cf51e687cd8032b5d8ff6f399b3e8f05d0b
ChenHaoJie9527 <a18520145493@163.com>
2024-07-07 11:12:00 +0800

    Logo

---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "../common/Logo";
+
+export default function HeaderTop() {
+  return (
+    <div className="flex w-full items-center">
+      <Logo />
+    </div>
+  );
+}
