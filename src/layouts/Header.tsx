c43f22a0561fdcd437bf2391a9d77589938f9b0a
ChenHaoJie9527 <a18520145493@163.com>
2024-07-07 11:11:52 +0800

    Logo

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Background from "../common/Background";
 
-export default function Header() {
+interface Props {
+  children: React.ReactNode;
+}
+export default function Header({ children }: Props) {
   return (
     <header className="bg-[#0d6af5] w-full">
       <Background />
+      {children}
     </header>
   );
 }
